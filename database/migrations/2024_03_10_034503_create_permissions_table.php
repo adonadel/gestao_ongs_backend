99a33769bfb45c1511f9c39e5fd1bab126a73e67
Alexandre Donadel <xanded.fer2@gmail.com>
2024-04-28 15:44:15 -0300

    little fix

--- database/migrations/2024_03_10_034503_create_permissions_table.php
+++ database/migrations/2024_03_10_034503_create_permissions_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('display_name');
-            $table->string('status', PermissionStatusEnum::toArrayWithString())->default(PermissionStatusEnum::ENABLED);
+            $table->enum('status', PermissionStatusEnum::toArrayWithString())->default(PermissionStatusEnum::ENABLED);
             $table->timestamps();
             $table->softDeletes();
         });
