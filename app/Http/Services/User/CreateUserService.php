3a3c7a8225c26406ef36f9e136c4925e035c9608
Alexandre Donadel <xanded.fer2@gmail.com>
2024-05-26 15:00:31 -0300

    ebaa-72: alterando forma que endereço é salvo no usuário

--- app/Http/Services/User/CreateUserService.php
+++ app/Http/Services/User/CreateUserService.php
@@ -3,6 +3,7 @@
 namespace App\Http\Services\User;
 
 
+use App\Repositories\AddressRepository;
 use App\Repositories\PeopleRepository;
 use App\Repositories\UserRepository;
 
@@ -13,10 +14,33 @@ class CreateUserService
         $userRepository = new UserRepository();
         $personRepository = new PeopleRepository();
 
-        $person = $personRepository->create(data_get($data, 'person'));
+        $personData = data_get($data, 'person');
+
+        if($addressData = data_get($personData, 'address')) {
+            $address = $this->handleAddress($addressData);
+            $personData['address_id'] = $address->id;
+            unset($personData['address']);
+        }
+
+
+        $person = $personRepository->create($personData);
 
         $data['people_id'] = $person->id;
 
         return $userRepository->create($data);
+    }
+
+    private function handleAddress(array $addressData)
+    {
+        $addressRepository = new AddressRepository();
+
+        if ($id = data_get($addressData, 'id')) {
+            $address = $addressRepository->getById($id);
+            $addressRepository->update($address, $addressData);
+        }else {
+            $address = $addressRepository->create($addressData);
+        }
+
+        return $address;
     }
 }
