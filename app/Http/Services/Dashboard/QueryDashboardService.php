d0dbd3c05705773be15d7ce0261e1089c0664b6e
Alexandre Donadel <xanded.fer2@gmail.com>
2024-06-20 23:37:01 -0300

    ebaa-123: alterando parte do período do filtro de dashboard

--- app/Http/Services/Dashboard/QueryDashboardService.php
+++ app/Http/Services/Dashboard/QueryDashboardService.php
@@ -11,24 +11,28 @@ use Illuminate\Support\Facades\DB;
 
 class QueryDashboardService
 {
+    private $initial;
+    private $final;
+
     public function getAnimalsTotal(array $filters)
     {
-        $initial = data_get($filters, 'initial');
-        $final = data_get($filters, 'final');
+        $type = data_get($filters, 'type', 'yearly');
+
+        $this->handleType($type);
 
         $animalRepository = new AnimalRepository();
 
         $totalAnimals = $animalRepository->newQuery()
-            ->when($initial && $final, function ($query) use ($initial, $final) {
-                $query->whereBetween('created_at', [$initial, $final]);
+            ->when($this->initial && $this->final, function ($query){
+                $query->whereBetween('created_at', [$this->initial, $this->final]);
             })
             ->count();
 
         $totalAdopted = $animalRepository->newQuery()
-            ->leftJoin('adoptions', 'animals.id', '=', 'adoptions.animal_id')
+            ->join('adoptions', 'animals.id', '=', 'adoptions.animal_id')
             ->where('adoptions.status', AdoptionsStatusEnum::ADOPTED)
-            ->when($initial && $final, function ($query) use ($initial, $final) {
-                $query->whereBetween('animal.created_at', [$initial, $final]);
+            ->when($this->initial && $this->final, function ($query){
+                $query->whereBetween('animals.created_at', [$this->initial, $this->final]);
             })
             ->count();
 
@@ -41,29 +45,30 @@ class QueryDashboardService
 
     public function getAnimalsTotalCastration(array $filters)
     {
-        $initial = data_get($filters, 'initial');
-        $final = data_get($filters, 'final');
+        $type = data_get($filters, 'type', 'yearly');
+
+        $this->handleType($type);
 
         $animalRepository = new AnimalRepository();
 
         $totalCastrated = $animalRepository->newQuery()
             ->where('castrate_type', AnimalCastrateEnum::CASTRATED)
-            ->when($initial && $final, function ($query) use ($initial, $final) {
-                $query->whereBetween('created_at', [$initial, $final]);
+            ->when($this->initial && $this->final, function ($query){
+                $query->whereBetween('created_at', [$this->initial, $this->final]);
             })
             ->count();
 
         $totalNotCastrated = $animalRepository->newQuery()
             ->where('castrate_type', AnimalCastrateEnum::NOT_CASTRATED)
-            ->when($initial && $final, function ($query) use ($initial, $final) {
-                $query->whereBetween('created_at', [$initial, $final]);
+            ->when($this->initial && $this->final, function ($query){
+                $query->whereBetween('created_at', [$this->initial, $this->final]);
             })
             ->count();
 
         $totalAwaitingCastration = $animalRepository->newQuery()
             ->where('castrate_type', AnimalCastrateEnum::AWAITING_CASTRATION)
-            ->when($initial && $final, function ($query) use ($initial, $final) {
-                $query->whereBetween('created_at', [$initial, $final]);
+            ->when($this->initial && $this->final, function ($query){
+                $query->whereBetween('created_at', [$this->initial, $this->final]);
             })
             ->count();
 
@@ -76,24 +81,25 @@ class QueryDashboardService
 
     public function getFinancesTotal(array $filters)
     {
-        $initial = data_get($filters, 'initial');
-        $final = data_get($filters, 'final');
+        $type = data_get($filters, 'type', 'yearly');
+
+        $this->handleType($type);
 
         $financeRepository = new FinanceRepository();
 
         $totalIncome = $financeRepository->newQuery()
             ->select(DB::raw('SUM(value) as total'))
             ->where('type', FinanceTypeEnum::INCOME)
-            ->when($initial && $final, function ($query) use ($initial, $final) {
-                $query->whereBetween('created_at', [$initial, $final]);
+            ->when($this->initial && $this->final, function ($query){
+                $query->whereBetween('created_at', [$this->initial, $this->final]);
             })
             ->first();
 
         $totalExpense = $financeRepository->newQuery()
             ->select(DB::raw('SUM(value) as total'))
             ->where('type', FinanceTypeEnum::EXPENSE)
-            ->when($initial && $final, function ($query) use ($initial, $final) {
-                $query->whereBetween('created_at', [$initial, $final]);
+            ->when($this->initial && $this->final, function ($query){
+                $query->whereBetween('created_at', [$this->initial, $this->final]);
             })
             ->first();
 
@@ -102,5 +108,26 @@ class QueryDashboardService
             'totalIncome' => $totalIncome->total,
             'totalExpense' => $totalExpense->total,
         ];
+    }
+
+    private function handleType(string $type)
+    {
+        switch ($type) {
+            case 'yearly':
+                $this->initial = now()->year.'-01-01';
+                $this->final = now()->year.'-12-31';
+                break;
+            case 'monthly':
+                $this->initial = now()->month.'-01-01';
+                $this->final = now()->month.'-12-'.now()->month( now()->month)->daysInMonth;
+                break;
+            case 'weekly':
+                $this->initial = now()->subDays(7)->format('Y-m-d');
+                $this->final = now()->format('Y-m-d');
+                break;
+            case 'all':
+                $this->initial = null;
+                $this->final = null;
+        }
     }
 }
